93fc5bf7566b69f845e50a07d478c4aa761bfc9b
HitecMedia Developer <developer@hitec.id>
2026-07-30 00:08:35 +0700

    fix: strip crossorigin attribute from CSS link tags to prevent obscure CORS/cache failures on Android Chrome

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
+// Plugin to strip crossorigin from CSS links to prevent mobile cache/CORS bugs
+const stripCssCrossoriginPlugin = () => {
+  return {
+    name: 'strip-css-crossorigin',
+    transformIndexHtml(html) {
+      return html.replace(/<link rel="stylesheet" crossorigin([^>]+)>/g, '<link rel="stylesheet"$1>');
+    }
+  }
+};
+
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react(), stripCssCrossoriginPlugin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -18,6 +27,7 @@ export default defineConfig({
     target: 'es2015',
     sourcemap: true,
     cssCodeSplit: false,
+    crossorigin: false,
     rollupOptions: {
       output: {
         entryFileNames: 'assets/[name].[hash].js',
